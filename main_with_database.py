bd41f412268ad77e232249f03a230f876b0ea8e3
Ratih <ratih.budi97@gmail.com>
2024-10-28 11:25:57 +0700

    Combine with database (Add Update Product Stocks Feature)

--- main_with_database.py
+++ main_with_database.py
@@ -155,5 +155,135 @@ def edit_product(conn):
 
 edit_product(conn)
 
+# Update Product Stocks Feature 
+def update_product_stocks(conn):
+  isiteratestock = True
+  isiterateprice = True
+  print("Update Product Stocks")
+  search_medicine_product_id = input("Enter Product ID: ")
+  # SQL SELECT query to find the product
+  select_product_query = """
+  SELECT * FROM Stock
+  WHERE productId = %s
+  """
+  cursor = conn.cursor()
+  cursor.execute(select_product_query, (search_medicine_product_id))
+  result = cursor.fetchone()
+  if result:
+    isfound = True
+    productId = result[2]
+    print("Product is found")
+    current_stocks = result[0]  # Assuming amount is at index 0
+    update_stocks_confirmation = input("What do you want to do? (1. Inbound stocks or 2. Outbound stocks): ")
+    while update_stocks_confirmation not in ["1", "2"]:
+      print("Wrong input. Try again")
+      update_stocks_confirmation = input("What do you want to do? (1. Inbound stocks or 2. Outbound stocks): ")
+      if update_stocks_confirmation == "1":
+        inbound_medicine_stocks = int(input("Enter inbound medicine stocks: "))
+        new_stock = current_stocks + inbound_medicine_stocks
+        update_stock_query = """
+        UPDATE Stock
+        SET amount = %s
+        WHERE productId = %s
+        """
+        cursor.execute(update_stock_query, (new_stock, productId))
+        conn.commit()
+        print("Successfully updated " + search_medicine_name + " stocks into " + str(new_stock))
+
+
+  while isiteratestock == True:
+    try:
+      new_medicine_stock = int(input("Enter new stock: "))
+      isiteratestock = False
+      break
+    except ValueError:
+      print("Invalid input. Please enter a valid new stock in integer.")
+  supplier = validate_supplier(conn, new_medicine_supplier_id)
+  if len(supplier) == 1:
+      insert_query = """
+      INSERT INTO Product (productID, productName, productStock, productPrice, supplierID)
+      VALUES (%s, %s, %s, %s, %s)
+      """ # SQL INSERT query
+      cursor = conn.cursor()
+      cursor.execute(insert_query, (new_medicine_product_id, new_medicine_name, new_medicine_stock, new_medicine_price, new_medicine_supplier_id))
+      conn.commit()
+      print("Successfully created " + new_medicine_name) # Execute the query with the new product data
+  else:
+      print("Supplier not found. Please add supplier first")
+      add_new_supplier(conn)
+
+add_new_product_to_db(conn)
+
+
+
+
+
+
+
+
+
+def update_product_stocks(conn):
+  isfound = False
+  print("Update Product Stocks")
+  search_medicine_product_id = input("Enter Product ID: ")
+  search_medicine_name = input("Enter medicine name: ")
+
+  # SQL SELECT query to find the product
+  select_product_query = """
+  SELECT * FROM Product
+  WHERE productId = %s AND productName = %s
+  """
+
+  cursor = conn.cursor()
+  cursor.execute(select_product_query, (search_medicine_product_id, search_medicine_name))
+  result = cursor.fetchone()
+
+  if result:
+    isfound = True
+    productId = result[3]
+    # SQL SELECT query to get the current amount from the Stock table
+    select_stock_query = """
+    SELECT amount 
+    FROM Stock 
+    WHERE productId = %s
+    """
+    cursor.execute(select_stock_query, (productId))
+    stock_result = cursor.fetchone()
+    if stock_result:
+      current_stocks = stock_result[0]  # Assuming amount is at index 0
+      update_stocks_confirmation = input("What do you want to do? (1. Inbound stocks or 2. Outbound stocks): ")
+      while update_stocks_confirmation not in ["1", "2"]:
+        print("Wrong input. Try again")
+        update_stocks_confirmation = input("What do you want to do? (1. Inbound stocks or 2. Outbound stocks): ")
+        if update_stocks_confirmation == "1":
+          inbound_medicine_stocks = int(input("Enter inbound medicine stocks: "))
+          new_stock = current_stocks + inbound_medicine_stocks
+          update_stock_query = """
+          UPDATE Stock
+          SET amount = %s
+          WHERE productId = %s
+          """
+          cursor.execute(update_stock_query, (new_stock, productId))
+          conn.commit()
+          print("Successfully updated " + search_medicine_name + " stocks into " + str(new_stock))
+        elif update_stocks_confirmation == "2":
+          outbound_medicine_stocks = int(input("Enter outbound medicine stocks: "))
+          new_stock = current_stocks - outbound_medicine_stocks
+          update_stock_query = """
+          UPDATE Stock
+          SET amount = %s
+          WHERE productId = %s
+          """
+          cursor.execute(update_stock_query, (new_stock, productId))
+          conn.commit()
+          print("Successfully updated " + search_medicine_name + " stocks into " + str(new_stock))
+    else:
+      print("Stock record not found for the product.")
+  else:
+     print("Product not found.")
+
+update_product_stocks(conn)
+
+
 
 
